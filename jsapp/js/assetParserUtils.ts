50fb4e45f53bb5540187a143b74433a424b1999c
Agus Hilman <gushil@gmail.com>
2024-05-21 18:31:40 +0700

    OC-22822 Fixed Form ID and Form version not displayed

--- jsapp/js/assetParserUtils.ts
+++ jsapp/js/assetParserUtils.ts
@@ -16,12 +16,15 @@ function parseSettings(asset: AssetResponse) {
     let foundSettings: AssetContentSettings = {};
     if (Array.isArray(settings) && settings.length) {
       foundSettings = settings[0];
+    } else {
+      foundSettings = <AssetContentSettings> settings;
     }
     return {
       unparsed__settings: foundSettings,
-      settings__style: foundSettings.style,
-      settings__form_id: foundSettings.form_id,
       settings__title: foundSettings.title,
+      settings__style: foundSettings.style !== undefined ? foundSettings.style : '',
+      settings__form_id: foundSettings.form_id !== undefined ? foundSettings.form_id : (foundSettings.id_string !== undefined ? foundSettings.id_string : ''),
+      settings__version: foundSettings.version !== undefined ? foundSettings.version : '',
     };
   } else {
     return {};
