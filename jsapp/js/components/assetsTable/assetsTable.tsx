4bca350c9b42adb74a790b1f31253dae5df19b14
Agus Hilman <gushil@gmail.com>
2026-06-22 17:29:01 +0700

    OC-27826: Hide assets table pagination

--- jsapp/js/components/assetsTable/assetsTable.tsx
+++ jsapp/js/components/assetsTable/assetsTable.tsx
@@ -350,7 +350,8 @@ export default class AssetsTable extends React.Component<AssetsTableProps, Asset
           <span>{t('##count## items').replace('##count##', String(this.props.totalAssets))}</span>
         )}
 
-        {this.renderPagination()}
+        {/* OpenClinica: Pagination intentionally hidden. */}
+        {/* {this.renderPagination()} */}
 
         {/* OpenClinica: Toggle fullscreen button intentionally hidden. */}
         {/* {this.props.totalAssets !== null && (
